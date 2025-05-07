f98408c3b3be4b48428117a04d05a02ef05d1430
Himmel221 <vincentbien187#gmail.com>
2025-05-07 22:40:18 +0800

    mwamwaaaa

--- client/src/pages/BuyArea.js
+++ client/src/pages/BuyArea.js
@@ -23,10 +23,11 @@ const BuyArea = () => {
   const navigate = useNavigate();
   const [searchTerm, setSearchTerm] = useState(""); 
   
+  const apiUrl = process.env.REACT_APP_API_URL;
 
   const fetchListings = useCallback(async () => {
     try {
-      const response = await axios.get('http://localhost:5000/api/listings', {
+      const response = await axios.get(`${apiUrl}/api/listings`, {
         headers: { Authorization: `Bearer ${token}` },
       });
 
@@ -85,7 +86,7 @@ const BuyArea = () => {
   const handleAddToCart = async () => {
     try {
       const response = await axios.post(
-        'http://localhost:5000/api/cart/add',
+        `${apiUrl}/api/cart/add`,
         {
           productId: selectedProduct._id,
           quantity: cartQuantity,
